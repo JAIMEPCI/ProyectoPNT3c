c2a3b0a3cd716b7f5ef8397fe7857311f836c649
XxJaimePCIxX <jaimeolarte888@gmail.com>
2023-05-29 00:31:20 -0500

    los archivos importantes puestos

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BibliotecaApp } from './BibliotecaApp';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<BibliotecaApp/>)
